bbcf4acd52fbd9a57d30c4efefcadd8d7d4e77da
“manabuyanagisawa <114840008+manabuyanagisawa@users.noreply.github.com>
2023-03-15 21:49:15 +0900

    更新

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->comment('名前');
-            $table->string('email')->unique()->comment('メールアドレス');
+            $table->string('email')->charset('utf8')->unique()->comment('メールアドレス');
             $table->timestamp('email_verified_at')->nullable()->comment('メールアドレス確認日時');
             $table->string('password')->comment('パスワード');
             $table->tinyInteger('role')->default('1')->comment('権限');
